8c8dd5e901e53304bdc192e955bc32da2205e8d6
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-09-16 09:50:09 +0100

    Update examStatistics.py

--- examStatistics.py
+++ examStatistics.py
@@ -2,7 +2,7 @@ grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 
 def print_grades(grades_input):  
     """Print each grade from the list."""
-    for grade in grades_input:
+    for grade in grades_input: 
         print(grade)  
     
 def grades_sum(scores):
